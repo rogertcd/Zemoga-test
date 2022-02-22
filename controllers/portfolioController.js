648124268dd349587c863a10a606e2c4673e5fea
rogertcd <rogertcd@msn.com>
2022-02-22 05:51:52 -0400

    Verify the correct parameters

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -11,30 +11,35 @@ AWS.config.update({
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = 'portfolio';
 
-exports.createOrUpdate = async (req, res) => {
-    const idPortfolio = req.body.id;
+exports.createOrUpdate = async (req, res, next) => {
     // console.log('idPortfolio', idPortfolio, 'params', req.headers.id);
-    if (idPortfolio !== '' && req.body.name !== undefined && req.body.twitterUser !== undefined) {
-        const newItem = {
-            "id": idPortfolio,
-            "experience": (req.body.experience === undefined) ? '' : req.body.experience,
-            "imagePath": (req.body.imagePath === undefined) ? '' : req.body.imagePath,
-            "name": req.body.name,
-            "twitterUser": req.body.twitterUser,
-            "email": (req.body.email === undefined) ? '' : req.body.email,
-            "address": (req.body.address === undefined) ? '' : req.body.address,
-            "phone": (req.body.phone === undefined) ? '' : req.body.phone,
-            "zipCode": (req.body.zipCode === undefined) ? '' : req.body.zipCode
+    try{
+        if ((req.body.id !== undefined && req.body.id !== '') && req.body.name !== undefined && req.body.twitterUser !== undefined) {
+            const newItem = {
+                "id": req.body.id,
+                "experience": (req.body.experience === undefined) ? '' : req.body.experience,
+                "imagePath": (req.body.imagePath === undefined) ? '' : req.body.imagePath,
+                "name": req.body.name,
+                "twitterUser": req.body.twitterUser,
+                "email": (req.body.email === undefined) ? '' : req.body.email,
+                "address": (req.body.address === undefined) ? '' : req.body.address,
+                "phone": (req.body.phone === undefined) ? '' : req.body.phone,
+                "zipCode": (req.body.zipCode === undefined) ? '' : req.body.zipCode
+            }
+            const params = {
+                TableName: TABLE_NAME,
+                Item: newItem
+            }
+            let respuesta = await dynamoClient.put(params).promise();
+            response.success(req, res, respuesta, 200);
+        } else {
+            response.error(req, res, "Datos no válidos", 400);
         }
-        const params = {
-            TableName: TABLE_NAME,
-            Item: newItem
-        }
-        let respuesta = await dynamoClient.put(params).promise();
-        response.success(req, res, respuesta, 200);
-    } else {
-        response.error(req, res, "Datos no válidos", 401);
+    } catch (e) {
+        console.log(e);
+        response.error(req, res, "Error interno", 500);
     }
+    next();
 };
 
 exports.getElementById = async (req, res) => {
